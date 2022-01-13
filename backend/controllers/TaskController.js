b3c7f76666dd5ed0ea076fd2a3a1edfa0c21065b
Andrey Poluyanov <dev.andrejpoluyanov@gmail.com>
2022-01-14 00:58:27 +0500

    Расширение функционала Task контроллера

--- backend/controllers/TaskController.js
+++ backend/controllers/TaskController.js
@@ -1,27 +1,36 @@
 import mongoose from "mongoose";
 import { taskSchema } from "../models/TaskModel.js";
+import jwt from 'jsonwebtoken';
 
 const { model } = mongoose;
 const Task = model('Task', taskSchema);
 
-const getTaskById = (req, res, next) => {
-    Task.findById(req.body.id, (err, r) => {
-        if (r) res.send({...r._doc, success: true });
-        else res.send({ success: false });
-    });
+const getTaskById = (req, res) => {
+    const { authorization } = req.headers;
+    const { id } = req.body;
+
+    if (!authorization) return res.status(403).json({ status: false, message: 'Пользователь не авторизован' });
+
+    try {
+        const token = authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+    
+        Task.findById(id, (err, r) => {
+            if (r && r.author._id !== decodedToken.id) res.status(200).json({ ...r._doc, success: true });
+            else res.status(400).json({ success: false, message: err });
+        });
+    } catch(err) {
+        res.status(403).json({ status: false, message: 'Ошибка токена' });
+    }
 }
 
-async function saveTask(req, res, next) {
-    const task = new Task({
-        name: req.body.name,
-        description: req.body.description,
-        color: req.body.color,
-        author: req.body.author
-    });
+ const saveTask = (req, res) => {
+    const { name, description, color, author } = req.body;
+    const task = new Task({ name, description, color, author });
 
-    await task.save().then(r => {
-        if (r === task) res.send({ success: true });
-        else res.send({ success: false });
+    task.save().then(r => {
+        if (r === task) res.status(200).json({ success: true, message: 'Задача успешно создана' });
+        else res.status(400).json({ success: false, message: 'Ошибка сохранения задачи' });
     });
 }
 
