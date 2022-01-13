90ab162672ed3da6c3745e76d40e329b87d5af8a
Andrey Poluyanov <dev.andrejpoluyanov@gmail.com>
2022-01-14 00:59:27 +0500

    Добавление роутинга User

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ if (result.error)
 import express from 'express';
 import bodyParser from 'body-parser';
 import { task } from './routes/TaskRoute.js'
+import { user } from './routes/UserRoute.js'
 
 const app = express();
 
@@ -15,9 +16,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(task);
+app.use(user);
 
 try {
-    await mongoose.connect(process.env.MONGODB_SERVER);
+    await mongoose.connect(process.env.MONGODB_SERVER, { useUnifiedTopology: true, useNewUrlParser: true });
     app.listen(process.env.SERVER_PORT);
 } catch (e) {
     console.error(e);
