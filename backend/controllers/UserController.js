3f643a0648cf4e63b658b320a11bf7fc134d0aee
Andrey Poluyanov <dev.andrejpoluyanov@gmail.com>
2022-01-15 18:25:28 +0500

    Рефакторинг кода функции регистрации

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,28 +1,34 @@
-import mongoose from "mongoose";
-import { userSchema } from "../models/UserModel.js";
 import bcrypt from "bcryptjs";
 import jwt from 'jsonwebtoken';
+import { User } from "../models/UserModel.js";
+import { Token } from "../models/TokenModel.js";
+import { Exception } from "../utils/Exception.js";
 
-const { model } = mongoose;
-const User = model('User', userSchema);
-
-const registration = (req, res) => {
-    const { login, password } = req.body;
-
-    User.findOne({ login }, (err, r) => {
-        if (r) return res.status(400).json({ 
-            status: false,
-            massage: 'Пользователь с текущим логином уже зарегистрирован'
-        });
-
+const registration = (req, res, next) => {
+    try {
+        const { name, email, password } = req.body;
         const hashPassword = bcrypt.hashSync(password, 10);
-        const user = new User({ login, password: hashPassword });
     
-        user.save().then(r => {
-            if (r === user) res.status(200).json({ success: true, message: 'Пользователь успешно зарегистрирован' });
-            else res.status(400).json({ success: false, message: 'Ошибка регистрации пользователя' });
+        User.create({ name, email, password: hashPassword }, (err, dataUser) => {
+            if (err) throw Exception.UnauthorizedError();
+
+            const payload = {
+                name: dataUser.name,
+                login: dataUser.login
+            };
+
+            const accessToken = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '30m' });
+            const refreshToken = jwt.sign(payload, process.env.SECRET_KEY_REFRESH, { expiresIn: '30d' });
+
+            Token.create({ user: dataUser._id, tokens: { access: accessToken, refresh: refreshToken }}, (err, dataToken) => {
+                if (err) throw Exception.UnauthorizedError();
+
+                return res.status(200).json({ token: dataToken.tokens.access });
+            });
         });
-    });
+    } catch (e) {
+        next(e);
+    }
 }
 
 const login = (req, res) => {
@@ -38,8 +44,7 @@ const login = (req, res) => {
 
         if (!isValidPassword) return res.status(400).json({ success: false, message: 'Введен неверный пароль' });
 
-        const token = jwt.sign({ id: r._id }, process.env.SECRET_KEY, { expiresIn: '6h' });
-
+        const token = jwt.sign({ id: r._id }, process.env.SECRET_KEY, { expiresIn: '30min' });
         res.status(200).json({ success: true, token });
     });
 }
