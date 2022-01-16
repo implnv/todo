b5465ff3055b4874cac3a887e825a7c985f921eb
Andrey Poluyanov <dev.andrejpoluyanov@gmail.com>
2022-01-16 14:58:47 +0500

    Рефакторинг кода контроллера

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,15 +2,50 @@ import bcrypt from "bcryptjs";
 import jwt from 'jsonwebtoken';
 import { User } from "../models/UserModel.js";
 import { Token } from "../models/TokenModel.js";
-import { Exception } from "../utils/Exception.js";
 
-const registration = (req, res, next) => {
+const registration = (req, res) =>  {
     try {
-        const { name, email, password } = req.body;
+        const { name, login, password } = req.body;
         const hashPassword = bcrypt.hashSync(password, 10);
     
-        User.create({ name, email, password: hashPassword }, (err, dataUser) => {
-            if (err) throw Exception.UnauthorizedError();
+        User.findOne({ login }, (err, dataUser) => {
+            if (err) return res.status(500).json({ message: 'Непредвиденная ошибка запроса' }); 
+            if (dataUser) return res.status(200).json({ message: 'Пользователь зарегистрирован в системе' }); 
+
+            User.create({ name, login, password: hashPassword }, (err, dataUser) => {
+                if (err) return res.status(500).json({ message: 'Непредвиденная ошибка запроса' });
+        
+                const payload = {
+                    name: dataUser.name,
+                    login: dataUser.login
+                };
+        
+                const accessToken = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '30m' });
+                const refreshToken = jwt.sign(payload, process.env.SECRET_KEY_REFRESH, { expiresIn: '30d' });
+        
+                Token.create({ user: dataUser._id, refresh: refreshToken }, (err, dataToken) => {
+                    if (err) return res.status(500).json({ message: 'Непредвиденная ошибка запроса' });
+        
+                    res.status(200).json({ user: { name: dataUser.name, login: dataUser.login }, tokens: { access: accessToken, refresh: dataToken.refresh } });
+                });
+            });
+        });
+    } catch (e) {
+        res.status(500).json({ message: 'Непредвиденная ошибка сервера' });
+    }
+}
+
+const login = (req, res) => {
+    try {
+        const { login, password } = req.body;
+
+        User.findOne({ login }, (err, dataUser) => {
+            if (err) return res.status(500).json({ message: 'Непредвиденная ошибка запроса' }); 
+            if (!dataUser) return res.status(200).json({ message: 'Пользователь не зарегистрирован в системе' }); 
+
+            const isValidPassword = bcrypt.compareSync(password, dataUser.password);
+
+            if (!isValidPassword) return res.status(400).json({ success: false, message: 'Введен неверный пароль' });
 
             const payload = {
                 name: dataUser.name,
@@ -18,35 +53,16 @@ const registration = (req, res, next) => {
             };
 
             const accessToken = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '30m' });
-            const refreshToken = jwt.sign(payload, process.env.SECRET_KEY_REFRESH, { expiresIn: '30d' });
 
-            Token.create({ user: dataUser._id, tokens: { access: accessToken, refresh: refreshToken }}, (err, dataToken) => {
-                if (err) throw Exception.UnauthorizedError();
+            Token.findOne({ user: dataUser._id }, (err, dataToken) => {
+                if (err) return res.status(500).json({ message: 'Непредвиденная ошибка запроса' });
 
-                return res.status(200).json({ token: dataToken.tokens.access });
+                res.status(200).json({ user: { name: dataUser.name, login: dataUser.login, tasks: dataUser.tasks }, tokens: { access: accessToken, refresh: dataToken.refresh } });
             });
         });
     } catch (e) {
-        next(e);
+        res.status(500).json({ message: 'Непредвиденная ошибка сервера' });
     }
-}
-
-const login = (req, res) => {
-    const { login, password } = req.body;
-
-    User.findOne({ login }, (err, r) => {
-        if (!r) return res.status(400).json({ 
-            status: false,
-            massage: 'Пользователь с текущим логином не найден'
-        });
-
-        const isValidPassword = bcrypt.compareSync(password, r.password);
-
-        if (!isValidPassword) return res.status(400).json({ success: false, message: 'Введен неверный пароль' });
-
-        const token = jwt.sign({ id: r._id }, process.env.SECRET_KEY, { expiresIn: '30min' });
-        res.status(200).json({ success: true, token });
-    });
 }
 
 export { registration, login }
