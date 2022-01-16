1c6ba215d6f30c8ceb9469d484ba55d869d64dfb
Andrey Poluyanov <dev.andrejpoluyanov@gmail.com>
2022-01-16 15:20:26 +0500

    Добавлена валидация данных

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 import { registration, login } from '../controllers/UserController.js';
+import { body } from 'express-validator'; 
 
 const user = Router();
 
-user.post('/api/user/registration', registration);
-user.post('/api/user/login', login);
+user.post('/api/user/registration', 
+                                    body('login').isEmail(), 
+                                    body('password').isLength({ min: 4, max: 8 }), 
+                                    registration);
+user.post('/api/user/login', 
+                                    body('login').isEmail(), 
+                                    body('password').isLength({ min: 4, max: 8 }), 
+                                    login);
 
-export { user };
+export { user }
