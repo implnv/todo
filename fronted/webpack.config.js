3240d67de000f55a51dfb66a08281e9af06843b5
Andrey Poluyanov <dev.andrejpoluyanov@gmail.com>
2022-01-21 18:56:33 +0500

    Конфигурация Webpack

---
+++ fronted/webpack.config.js
@@ -0,0 +1,37 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin');
+
+module.exports = {
+    entry: './src/index.jsx',
+    mode: 'development',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            loader: 'babel-loader',
+            options: { presets: ["@babel/env"] }
+        }, 
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        }]
+    },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new HtmlWebpackPartialsPlugin({
+            path: path.resolve(__dirname, 'src/root.html')
+        })
+    ],
+    devServer: {
+        static: './dist',
+        open: true,
+        historyApiFallback: true
+    }
+};
