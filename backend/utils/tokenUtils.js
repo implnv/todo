db335473f082494b95ed579375d37c8e811b0dec
Andrey Poluyanov <dev.andrejpoluyanov@gmail.com>
2022-02-06 01:40:00 +0500

    Добавление утилит

---
+++ backend/utils/tokenUtils.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+
+const tokenGenerate = (dataUser) => {
+    try {
+        const payload = {
+            id: dataUser.id,
+            name: dataUser.name,
+            login: dataUser.login
+        };
+    
+        const accessToken = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '30m' });
+        const refreshToken = jwt.sign(payload, process.env.SECRET_KEY_REFRESH, { expiresIn: '30d' });
+
+        return { accessToken, refreshToken }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+const verifyAccessToken = (token) => {
+    try {
+        const data = jwt.verify(token, process.env.SECRET_KEY);
+        return data;
+    } catch (e) {
+        console.log('Токен не валиден');
+    }
+}
+
+export { tokenGenerate, verifyAccessToken }
