caae9051d378fa9036ac22882a0ad62f02c8f8ac
Andrey Poluyanov <dev.andrejpoluyanov@gmail.com>
2022-02-07 10:05:28 +0500

    Исправление ошибки

--- backend/middleware/authorizationMiddleware.js
+++ backend/middleware/authorizationMiddleware.js
@@ -2,6 +2,8 @@ import { verifyAccessToken } from "../utils/tokenUtils.js";
 
 const authorizationMiddleware = (req, res, next) => {
     const bearer = req.headers.authorization;
+    if (!bearer) return res.status(500).json({ 'message': 'Пользователь не авторизован' });
+    
     const token = bearer.split(' ')[1];
 
     if (token === 'null') return res.status(500).json({ 'message': 'Токен не обнаружен' });
--- fronted/webpack.config.js
+++ fronted/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
     resolve: { extensions: ["*", ".js", ".jsx"] },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name].bundle.js'
     },
     plugins: [
         new HtmlWebpackPlugin({ title: 'TODOS' }),
